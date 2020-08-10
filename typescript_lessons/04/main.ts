74868a7d77d54b032e541131094d4bc23d101df2
fujisii <51264835+fujisii@users.noreply.github.com>
2020-08-10 14:55:37 +0900

    add 04_さまざまな型を使ってみよう

---
+++ typescript_lessons/04/main.ts
@@ -0,0 +1,19 @@
+// 型
+
+/**
+ * number
+ * string
+ * boolean
+ * any
+ */
+
+var i: number;
+var i: number = 10;
+var i = 10; // i: number
+
+var x; // var x: any
+x = 10;
+x = "hello";
+
+var results:number[];
+results = [10, 5, 3];
