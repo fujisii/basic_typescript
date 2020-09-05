01d24bab015fb45798708f81ce18364df316da1f
fujisii <51264835+fujisii@users.noreply.github.com>
2020-09-05 22:56:57 +0900

    add #16

---
+++ typescript_lessons/16_ジェネリクスを使ってみよう/main.ts
@@ -0,0 +1,21 @@
+/**
+ * #16 ジェネリクスを使ってみよう
+ * - Genericsとは抽象化されたデータ型のこと
+ * - Genericsはちょっと難しい概念だが、上手く使えば安全かつ効率的な設計ができるようになる
+ *
+ * @example
+ * $ cd /vagrant/typescript_lessons/
+ * $ cd 16_ジェネリクスを使ってみよう/
+ * $ tsc main.ts
+ * $ node main.js
+ */
+
+function getArray<T>(value: T): T[] {
+    // 1. 型を指定する部分はとりあえず「T」と書く
+    // 2. このTは別に何でもいいが、慣習的にTypeのTを使うことが多い
+    // 3. そうしたあと、関数名の右側に「<T>」を書いてあげると、
+    //    実行時に何の型に対して処理を行うかを指定できる
+    return [value, value, value];
+}
+console.log(getArray<number>(3));
+console.log(getArray<string>("hello"));
